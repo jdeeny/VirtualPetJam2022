3f7f95828703640e246df121a2a92e3a8b875af9
John Deeny <jdeeny@gmail.com>
2022-01-07 20:34:42 -0700

    add middleclass and a config

--- main.lua
+++ main.lua
@@ -1,3 +1,57 @@
+local class = require 'lib.middleclass'
+
+-- Should be the only global
+gameWorld = {}
+
+function love.load()
+  gameWorld.random = love.math.newRandomGenerator()
+  gameWorld.random:setSeed(os.time())
+  math.randomseed(os.time())
+
+--   gameWorld.settings = require('ui.settings'):new()
+--   gameWorld.settings:load()
+--   gameWorld.settings:save()
+
+--   gameWorld.assets = cargo.init('assets')
+--   gameWorld.colors = require('ui.colors')
+--   gameWorld.sound = require('ui.sound'):new()
+
+--   gameWorld.paletteswap = require('ui.paletteswap'):new()
+
+--   -- Comment this out to disable debug print
+--   --gameWorld.debug = require('ui.debug'):new()
+
+--   gameWorld.playerInput = require('player.controls')
+
+--   gameWorld.gameState = require('gamestates.manager'):new()
+--   gameWorld.playerData = require('player.playerdata'):new()
+--   gameWorld.playerData:reset()
+--   gameWorld.gameState:setState('splash')
+end
+
+function love.update(dt)
+--   flux.update(dt)
+--   gameWorld.gameState:update(dt)
+--   gameWorld.sound:update(dt)
+--   gameWorld.playerInput:update()  -- update the input immediately so everything else can use the up to date info
+--   if gameWorld.debug then gameWorld.debug:update(dt) end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+--   gameWorld.gameState:draw()
+--   if gameWorld.debug then gameWorld.debug:draw() end
+end
+
+function love.quit()
+end
+
+function love.focus(f)
+--   if not f and gameWorld.gameState:getCurrent() == 'gameplay' then
+--     gameWorld.gameState:pushState('pause')
+--   end
+end
+
+-- switch to a newly connected joystick
+function love.joystickadded(j)
+--  if gameWorld.playerInput then gameWorld.playerInput.joystick = j end
 end
